f1ecda763d1ebf2d638303af1910ae5836f91ca9
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-07-31 19:53:55 +0200

    bulk API requests

--- HelloMatomo.php
+++ HelloMatomo.php
@@ -13,6 +13,26 @@ $client = new Client([
   'base_uri' => 'https://analytics.doorinsider.com/',
 ]);
 
+$bulkRequests = [
+  [
+    'period' => 'day',
+    'date' => 'today',
+    'method' => 'Actions.getPageUrl',
+    'pageUrl' => '/fr/vente/chateau-12000-1277',
+  ],
+  [
+    'period' => 'day',
+    'date' => 'today',
+    'method' => 'UserCountry.getCity',
+    'segment' => 'pageUrl=@/fr/vente/chateau-12000-1277',
+  ],
+];
+$urls = [];
+foreach ($bulkRequests as $bulkRequest) {
+  $bulkRequest['idSite'] = 2;
+  $urls[] = http_build_query($bulkRequest);
+}
+
 $response = $client->request(
   'POST',
   '', [
@@ -20,19 +40,18 @@ $response = $client->request(
       'module' => 'API',
       'format' => 'json',
       'token_auth' => 'db1c7ef8a962ad64baf481145c7dc938',
-      'idSite' => 2,
-      'period' => 'day',
-      'date' => 'today',
-      'method' => 'Actions.getPageUrl',
-      'pageUrl' => '/fr/vente/chateau-12000-1277',
+      'method' => 'API.getBulkRequest',
+      'urls' => $urls,
     ],
   ]
 );
 
 if ($response->getStatusCode() === 200) {
-  $results = json_decode($response->getBody()->getContents());
-  foreach ($results as $result) {
-    dump($result);
-    print $result->label . "\n";
+  $bulkResults = json_decode($response->getBody()->getContents());
+  foreach ($bulkResults as $results) {
+    foreach ($results as $result) {
+      dump($result);
+      print $result->label . "\n";
+    }
   }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+# Matomo Reporting API requests
+
+````
+https://analytics.doorinsider.com/
+?module=API
+&format=json
+&token_auth=db1c7ef8a962ad64baf481145c7dc938
+&idSite=2
+&period=day
+&date=today
+&method=Actions.getPageUrl
+&pageUrl=
+````
+
+````
+https://analytics.doorinsider.com/
+?module=API
+&format=json
+&token_auth=db1c7ef8a962ad64baf481145c7dc938
+&idSite=2
+&period=day
+&date=today
+&method=UserCountry.getCity
+&segment=pageUrl=@%2Ffr%2Fvente%2Fchateau-12000-1277
+````
