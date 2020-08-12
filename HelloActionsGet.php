6066c82bbb99bbf6817daccb7ba034e45be6422b
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-08-12 15:52:42 +0200

    up

--- HelloActionsGet.php
+++ HelloActionsGet.php
@@ -16,20 +16,13 @@ $client = new Client([
 $bulkRequests = [
   [
     'period' => 'day',
-    'date' => '2020-08-01',
-    'method' => 'Actions.get',
-    'segment' => 'pageUrl=@-1278',
-  ],
-  [
-    'period' => 'day',
-    'date' => '2020-08-01',
-    'method' => 'Actions.getPageUrl',
-    'pageUrl' => '/fr/vente/appartement-91160-1278',
+    'date' => '2020-01-01,today',
+    'method' => 'Actions.getPageUrls',
   ],
 ];
 $urls = [];
 foreach ($bulkRequests as $bulkRequest) {
-  $bulkRequest['idSite'] = 1;
+  $bulkRequest['idSite'] = 9;
   $urls[] = http_build_query($bulkRequest);
 }
 
@@ -48,5 +41,24 @@ $response = $client->request(
 
 if ($response->getStatusCode() === 200) {
   $bulkResults = json_decode($response->getBody()->getContents());
-  dump($bulkResults);
+  $pageUrlsByDate = $bulkResults[0];
+  $pageUrlsData = [];
+  foreach ($pageUrlsByDate as $date => $pageUrlsOnDate) {
+    foreach ($pageUrlsOnDate as $key => $pageUrl) {
+      $pageUrlsData[$date][$key] = [
+        'segment' => $pageUrl->segment,
+        'nb_hits' => $pageUrl->nb_hits,
+        'sum_time_spent' => $pageUrl->sum_time_spent,
+      ];
+      if (property_exists($pageUrl, 'nb_uniq_visitors')) {
+        $pageUrlsData[$date][$key]['nb_uniq_visitors'] = $pageUrl->nb_uniq_visitors;
+      }
+      if (property_exists($pageUrl, 'exit_nb_visits')) {
+        $pageUrlsData[$date][$key]['exit_nb_visits'] = $pageUrl->exit_nb_visits;
+      }
+    }
+  }
+
+  $serialized = serialize($pageUrlsData);
+  print $serialized;
 }
---
+++ HelloCities.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Test.
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+  'base_uri' => 'https://analytics.doorinsider.com/',
+]);
+
+$bulkRequests = [
+  [
+    'period' => 'day',
+    'date' => '2020-08-06,today',
+    'method' => 'UserCountry.getCity',
+    'segment' => urlencode('pageUrl!='),
+  ],
+];
+$urls = [];
+foreach ($bulkRequests as $bulkRequest) {
+  $bulkRequest['idSite'] = 9;
+  $urls[] = http_build_query($bulkRequest);
+}
+
+$response = $client->request(
+  'POST',
+  '', [
+    'query' => [
+      'module' => 'API',
+      'format' => 'json',
+      'token_auth' => 'db1c7ef8a962ad64baf481145c7dc938',
+      'method' => 'API.getBulkRequest',
+      'urls' => $urls,
+    ],
+  ]
+);
+
+if ($response->getStatusCode() === 200) {
+  $bulkResults = json_decode($response->getBody()->getContents());
+  dump($bulkResults);
+  $citiesByDate = $bulkResults[0];
+  foreach ($citiesByDate as $date => $citiesOnDate) {
+    dump($citiesOnDate);
+  }
+  // $pageUrlsByDate = $bulkResults[0];
+  // $pageUrlsData = [];
+  // foreach ($pageUrlsByDate as $date => $pageUrlsOnDate) {
+  //   foreach ($pageUrlsOnDate as $key => $pageUrl) {
+  //     $pageUrlsData[$date][$key] = [
+  //       'segment' => $pageUrl->segment,
+  //       'nb_hits' => $pageUrl->nb_hits,
+  //       'sum_time_spent' => $pageUrl->sum_time_spent,
+  //     ];
+  //     if (property_exists($pageUrl, 'nb_uniq_visitors')) {
+  //       $pageUrlsData[$date][$key]['nb_uniq_visitors'] = $pageUrl->nb_uniq_visitors;
+  //     }
+  //     if (property_exists($pageUrl, 'exit_nb_visits')) {
+  //       $pageUrlsData[$date][$key]['exit_nb_visits'] = $pageUrl->exit_nb_visits;
+  //     }
+  //   }
+  // }
+
+  // $serialized = serialize($pageUrlsData);
+  // print $serialized;
+}
